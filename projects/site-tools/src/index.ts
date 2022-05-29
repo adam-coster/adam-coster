08f559987764821b048a8b2ac3c455660ed00d3e
Adam Coster <adam@bscotch.net>
2022-05-29 14:38:34 -0500

    fix: Article summaries and search index files need to exist prior to Vite loading the config.

--- projects/site-tools/src/index.ts
+++ projects/site-tools/src/index.ts
@@ -1,3 +1,4 @@
 export { createConfig } from './config.js';
+export { generateArticleSummaries } from './articles.js';
 export * from './types/Article.js';
 export * from './types/Utility.js';
--- projects/site/svelte.config.js
+++ projects/site/svelte.config.js
@@ -1,14 +1,22 @@
-import { createConfig } from '@adam-coster/site-tools';
+import {
+  createConfig,
+  generateArticleSummaries,
+} from '@adam-coster/site-tools';
 import { Pathy } from '@bscotch/pathy';
 import { fileURLToPath } from 'url';
 
 const root = new Pathy(fileURLToPath(import.meta.url)).up();
 
-const config = createConfig({
+const options = {
   staticDir: root.join('./static').absolute,
   articlesDir: root.join('./src/routes/articles').absolute,
   feedsDir: root.join('./static/feeds').absolute,
   productionBaseUrl: 'https://adamcoster.com',
-});
+};
+
+// Run during import to ensure that article summary files exist, to prevent downstream errors.
+generateArticleSummaries(options);
+
+const config = createConfig(options);
 
 export default config;
