b39873da8ffd2ba454503129113e9a7726771cf6
Adam Coster <adam@bscotch.net>
2022-08-14 18:39:30 -0500

    partial: Added some cypress tests, but currently cannot run as part of deployment.

---
+++ projects/site/cypress/e2e/blog-search.cy.js
@@ -0,0 +1,18 @@
+/// <reference types="cypress" />
+
+describe('Blog Search', () => {
+  beforeEach(() => {
+    cy.visit('/blog');
+  });
+
+  it('starts with all results', () => {
+    cy.get('.article-snippet').should('have.length.above', 20);
+  });
+
+  it('can submit a search', () => {
+    cy.get('#searchbar').type('title:commonjs').type('{enter}').wait(1000);
+    cy.get('.article-snippet>header>h2>a').each((el) => {
+      expect(el.text().toLowerCase()).to.contain('commonjs');
+    });
+  });
+});
