3f21788440c1dbfad1a575c1956ffd61a09ed97d
Adam Coster <adam@bscotch.net>
2022-10-09 18:26:41 -0500

    feat: Added externally-generated microdata to page routing

--- projects/site/src/routes/blog/[slug]/+page.ts
+++ projects/site/src/routes/blog/[slug]/+page.ts
@@ -8,7 +8,15 @@ export const csr = true;
 export const load: PageLoad = async (input) => {
 	const { slug } = input.params;
 	const articleSearcher = new ArticleSearcher(input.fetch);
-	const frontmatter = await articleSearcher.get(slug);
+
+	const [frontmatter, microdata] = await Promise.all([
+		articleSearcher.get(slug),
+		input
+			.fetch(`/microdata/blog/${slug}.json`)
+			.then((result) =>
+				result.status === 200 ? (result.json() as Promise<any[]>) : [],
+			),
+	]);
 	if (!frontmatter) {
 		throw error(404);
 	}
@@ -17,12 +25,15 @@ export const load: PageLoad = async (input) => {
 		description: frontmatter.description || 'An article by Adam Coster',
 		canonical: frontmatter.canonical,
 		type: 'article',
-		microdata: createArticleMicrodata({
-			headline: frontmatter.title,
-			datePublished: frontmatter.publishedAt,
-			dateModified: frontmatter.editedAt,
-			images: frontmatter.images,
-		}),
+		microdata: [
+			createArticleMicrodata({
+				headline: frontmatter.title,
+				datePublished: frontmatter.publishedAt,
+				dateModified: frontmatter.editedAt,
+				images: frontmatter.images,
+			}),
+			...microdata,
+		],
 	};
 
 	return {
