cb44d093a8c6ce42350b8419886ac641ea02c13c
Adam Coster <adam@bscotch.net>
2023-04-30 15:59:37 -0500

    meta: Resolved postinstall issue with fontawesome

--- .pnpmfile.cjs
+++ .pnpmfile.cjs
@@ -1,10 +1,4 @@
-const os = require('os');
-
 function readPackage(pkg) {
-	// The podcast package only works on Windows.
-	if (pkg.name === '@adam-coster/podcast-setup' && os.platform() !== 'win32') {
-		pkg.dependencies = {};
-	}
 	return pkg;
 }
 
