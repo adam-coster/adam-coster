be130b962a9b10f849bdd2c2f076aa99ffa9ebab
Adam Coster <adam@bscotch.net>
2023-11-26 13:29:21 -0600

    Revert "feat: Added Sentry"
    This reverts commit 0782dca96644374132c0ba4ec14d5a26044d2841.

--- projects/site/vite.config.ts
+++ projects/site/vite.config.ts
@@ -1,16 +1,10 @@
 import { updateArticleSearch } from '@adam-coster/site-tools';
-import { sentrySvelteKit } from "@sentry/sveltekit";
 import { sveltekit } from '@sveltejs/kit/vite';
 import type { UserConfig } from 'vite';
 import { options } from './project.config.js';
 
 const config: UserConfig = {
-    plugins: [sentrySvelteKit({
-        sourceMapsUploadOptions: {
-            org: "personal-96z",
-            project: "personal-site"
-        }
-    }), sveltekit(), updateArticleSearch(options)]
+	plugins: [sveltekit(), updateArticleSearch(options)],
 };
 
 export default config;
