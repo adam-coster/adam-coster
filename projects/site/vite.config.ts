0782dca96644374132c0ba4ec14d5a26044d2841
Adam Coster <adam@bscotch.net>
2023-11-26 13:14:50 -0600

    feat: Added Sentry

--- projects/site/vite.config.ts
+++ projects/site/vite.config.ts
@@ -1,10 +1,16 @@
 import { updateArticleSearch } from '@adam-coster/site-tools';
+import { sentrySvelteKit } from "@sentry/sveltekit";
 import { sveltekit } from '@sveltejs/kit/vite';
 import type { UserConfig } from 'vite';
 import { options } from './project.config.js';
 
 const config: UserConfig = {
-	plugins: [sveltekit(), updateArticleSearch(options)],
+    plugins: [sentrySvelteKit({
+        sourceMapsUploadOptions: {
+            org: "personal-96z",
+            project: "personal-site"
+        }
+    }), sveltekit(), updateArticleSearch(options)]
 };
 
 export default config;
