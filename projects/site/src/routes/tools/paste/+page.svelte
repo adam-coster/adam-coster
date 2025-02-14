8368c03078d9fa06058c9e10c0a7a49212513a58
Adam Coster <adam@bscotch.net>
2025-02-13 20:14:10 -0600

    feat: Added base functionality to the clipboard content viewer

--- projects/site/src/routes/tools/paste/+page.svelte
+++ projects/site/src/routes/tools/paste/+page.svelte
@@ -1,6 +1,16 @@
 <script lang="ts">
+	import hljs from 'highlight.js/lib/core';
+	import json from 'highlight.js/lib/languages/json';
+	import html from 'highlight.js/lib/languages/xml';
+
+	// Then register the languages you need
+	hljs.registerLanguage('json', json);
+	hljs.registerLanguage('html', html);
+
 	let nothing = $state(false);
-	let entries: { type: string }[] = $state([]);
+	let entries: { type: string; html: string }[] = $state([]);
+	let files: { name: string; type: string }[] = $state([]);
+
 	function onpaste(e: ClipboardEvent) {
 		// Reset
 		nothing = false;
@@ -13,15 +23,33 @@
 			nothing = true;
 			return;
 		}
+		for (const file of clipboardData.files) {
+			files.push({
+				name: file.name,
+				type: file.type,
+			});
+		}
 		for (const type of clipboardData.types) {
 			if (type === 'Files') {
-				const files = clipboardData.files;
-				// for(const file of files){
-				// 	entries.push({type: file.type});
-				// }
 				continue;
 			}
-			entries.push({ type });
+			const data = clipboardData.getData(type);
+			let html: string;
+			if (type === 'text/html') {
+				html = hljs.highlight(data, { language: 'html' }).value;
+			} else {
+				// Try to interpet as JSON
+				try {
+					const json = JSON.parse(data);
+					html = hljs.highlight(JSON.stringify(json, null, 2), {
+						language: 'json',
+					}).value;
+				} catch (e) {
+					// Just as-is
+					html = `<pre><code>${data}</code></pre>`;
+				}
+			}
+			entries.push({ type, html });
 		}
 	}
 </script>
@@ -35,11 +63,34 @@
 	<input autofocus {onpaste} placeholder="Paste here!" />
 	<output>
 		{#if nothing}
-			<p class="warn">Nothing in the clipboard!</p>
+			<p class="warning">Nothing in the clipboard!</p>
 		{/if}
-		{#each entries as entry}
-			<p>{entry.type}</p>
-		{/each}
+		{#if entries.length}
+			<h2>Text Content</h2>
+			<ul class="reset">
+				{#each entries as entry}
+					<li>
+						<article>
+							<h3>{entry.type}</h3>
+							<div class="pasted-text">{@html entry.html}</div>
+						</article>
+					</li>
+				{/each}
+			</ul>
+		{/if}
+		{#if files.length}
+			<h2>File Content</h2>
+			<ul class="reset">
+				{#each files as file}
+					<li>
+						<article>
+							<h3>{file.name}</h3>
+							<p>{file.type}</p>
+						</article>
+					</li>
+				{/each}
+			</ul>
+		{/if}
 	</output>
 </section>
 
@@ -56,5 +107,12 @@
 		aspect-ratio: 3/2;
 		border: 1px solid currentColor;
 		border-radius: 0.5em;
+	}
+	article {
+		width: 960px;
+		max-width: 100%;
+	}
+	article .pasted-text {
+		text-align: left;
 	}
 </style>
